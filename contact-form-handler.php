3e173c4202c97eda2a5e2aea67e5b721e3a7e704
AliceC <alycolbar@gmail.com>
2020-05-29 09:39:06 -0400

    add comment to php file

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,17 +1,18 @@
+
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['body'];
+//$name = $_POST['name'];
+//$visitor_email = $_POST['email'];
+//$message = $_POST['body'];
 
-$email_from = 'alycolbar@gmail.com';
-$email_subject = "New Form Submission";
-$email_body = "User Name: $name.\n".
-                "User mail: $visitor_email.\n".
-                    "User Message: $message.\n";
+//$email_from = 'alycolbar@gmail.com';
+//$email_subject = "New Form Submission";
+//$email_body = "User Name: $name.\n".
+  //              "User mail: $visitor_email.\n".
+    //                "User Message: $message.\n";
 
-$to = "palyncakes@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers = "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
-header("Location: index.html");
+//$to = "palyncakes@gmail.com";
+//$headers = "From: $email_from \r\n";
+//$headers = "Reply-To: $visitor_email \r\n";
+//mail($to,$email_subject,$email_body,$headers);
+//header("Location: index.html");
 ?>
